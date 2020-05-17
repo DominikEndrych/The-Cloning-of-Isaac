7caf66e33341044e291863ed993c4f4cafc2d37a
Dominik <endrych.d@seznam.cz>
2020-05-17 17:55:43 +0200

    Added player and basic movement
    Player can move and instantiate bullets.

---
+++ The Cloning of Isaac/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float speed = 1;
+    [SerializeField] Rigidbody2D player;
+
+    private Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        player.MovePosition(player.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
---
+++ The Cloning of Isaac/Assets/Scripts/ShootingController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingController : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;     // bullet placeholder
+
+    [SerializeField] Transform origin;
+
+    private float timeBetweenShots = 0.333f;    // 3 shots per second
+
+    private float timestamp;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= timestamp && Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(bullet, origin.position, transform.rotation);
+            timestamp = Time.time + timeBetweenShots;
+        }
+    }
+}
